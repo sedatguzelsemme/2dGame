7d8d04ac68f1e2e4b2660bfc4026909eea9c23b5
Sedat Güzelşemme <tfs@antasya.com>
2018-12-15 08:46:27 +0300

    base structure created

--- src/com/guzelsemme/Game.java
+++ src/com/guzelsemme/Game.java
@@ -7,6 +7,8 @@ public class Game implements Runnable {
     private Display display;
     private int width,height;
 
+    private boolean running = false;
+    
     private Thread thread;
 
     public Game(String title,int width,int height){
@@ -15,13 +17,53 @@ public class Game implements Runnable {
         display = new Display(title, width, height);
     }
 
+    /*
+     * this method will execute only at begining
+     */    
+    private void init() {
+    	
+    }
+    
     @Override
     public void run() {
-
+    	init();
+    	while(running) {
+    		tick();
+    		render();
+    	}
+    	stop();
     }
 
-    public synchronized void start(){
+    /*
+     * render everything
+     */
+    private void render() {
+		// TODO Auto-generated method stub
+		
+	}
+    /*
+     * update all variables
+     * scenes and everything
+     */
+	private void tick() {
+		// TODO Auto-generated method stub
+		
+	}
 
+	public synchronized void start(){
+    	
+    	thread = new Thread(this);
+    	thread.start();
+    	
+    }
+    
+    public synchronized void stop(){
+    	try {
+			thread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
 }
